8c4f612c7ef12ff1a9a3984a165b7bc731210def
Dmitry Dedov <me@dmitry.it>
2022-06-20 10:32:47 +0000

    set database url in runtime preparation for Heroku deployment

--- rent_assistant/apps/catalog/lib/catalog/repo.ex
+++ rent_assistant/apps/catalog/lib/catalog/repo.ex
@@ -2,4 +2,10 @@ defmodule Catalog.Repo do
   use Ecto.Repo,
     otp_app: :catalog,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    config = Keyword.put(config, :url, System.get_env("DATABASE_URL"))
+
+    {:ok, config}
+  end
 end
