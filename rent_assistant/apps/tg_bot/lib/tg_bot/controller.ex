74b8abf74e106aea0fbf537dac476f797bd6466c
Dmitry Dedov <me@dmitry.it>
2022-06-25 09:07:43 +0000

    add start/stop stub actions for a bot

---
+++ rent_assistant/apps/tg_bot/lib/tg_bot/controller.ex
@@ -0,0 +1,47 @@
+defmodule TgBot.Controller do
+  use Telegram.Bot
+
+  def handle_update(
+        %{
+          "message" => %{
+            "text" => "/start",
+            "chat" => %{"id" => chat_id},
+            "message_id" => message_id
+          }
+        },
+        token
+      ) do
+    if chat = TgBot.Repo.get_by(TgBot.Chat, %{chat_id: chat_id}) do
+      chat = Ecto.Changeset.change(chat, active: true)
+      TgBot.Repo.update(chat)
+    else
+      TgBot.Repo.insert(%TgBot.Chat{chat_id: chat_id, active: true})
+    end
+
+    Telegram.Api.request(token, "sendMessage",
+      chat_id: chat_id,
+      text: "I'll notify you about new listings"
+    )
+  end
+
+  def handle_update(
+        %{
+          "message" => %{
+            "text" => "/stop",
+            "chat" => %{"id" => chat_id},
+            "message_id" => message_id
+          }
+        },
+        token
+      ) do
+    if chat = TgBot.Repo.get_by(TgBot.Chat, %{chat_id: chat_id}) do
+      chat = Ecto.Changeset.change(chat, active: false)
+      TgBot.Repo.update(chat)
+    end
+
+    Telegram.Api.request(token, "sendMessage",
+      chat_id: chat_id,
+      text: "I'm stopping to notify you about new listings. Feel free to start me again."
+    )
+  end
+end
---
+++ rent_assistant/apps/tg_bot/test/tg_bot/controller_spec.exs
@@ -0,0 +1,4 @@
+defmodule TgBot.ControllerSpec do
+  xit "starts watching for new listings" do
+  end
+end
