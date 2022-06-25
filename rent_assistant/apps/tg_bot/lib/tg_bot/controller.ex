15e6c51512adde3873e0cbf09f4377d4fbf30849
Dmitry Dedov <me@dmitry.it>
2022-06-25 10:05:27 +0000

    "info" tg command

--- rent_assistant/apps/tg_bot/lib/tg_bot/controller.ex
+++ rent_assistant/apps/tg_bot/lib/tg_bot/controller.ex
@@ -44,4 +44,28 @@ defmodule TgBot.Controller do
       text: "I'm stopping to notify you about new listings. Feel free to start me again."
     )
   end
+
+  def handle_update(
+        %{
+          "message" => %{
+            "text" => "/info",
+            "chat" => %{"id" => chat_id},
+            "message_id" => message_id
+          }
+        },
+        token
+      ) do
+    chat = TgBot.Repo.get_by(TgBot.Chat, %{chat_id: chat_id})
+
+    info = "I'm currently #{if chat.active do
+      "notifying"
+    else
+      "not notifying"
+    end} about new listings"
+
+    Telegram.Api.request(token, "sendMessage",
+      chat_id: chat_id,
+      text: info
+    )
+  end
 end
