e7947e064861f402cc7745f1f75172841ae94924
Dmitry Dedov <me@dmitry.it>
2022-06-25 09:41:22 +0000

    run tg webhook handler for production environment

--- rent_assistant/apps/tg_bot/lib/tg_bot/application.ex
+++ rent_assistant/apps/tg_bot/lib/tg_bot/application.ex
@@ -7,10 +7,22 @@ defmodule TgBot.Application do
 
   @impl true
   def start(_type, _args) do
+    webhook_config = [
+      host: System.get_env("HOST"),
+      local_port: System.get_env("PORT") |> Kernel.||("3000") |> String.to_integer(),
+      set_webhook: false
+    ]
+
+    bot_config = [
+      token: System.get_env("BOT_TOKEN"),
+      max_bot_concurrency: 10
+    ]
+
     children = [
       # Starts a worker by calling: TgBot.Worker.start_link(arg)
       {TgBot.Repo, []},
-      {TgBot.EventsHandler, []}
+      {TgBot.EventsHandler, []},
+      {Telegram.Webhook, config: webhook_config, bots: [{TgBot.Controller, bot_config}]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
