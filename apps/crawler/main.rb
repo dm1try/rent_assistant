b1ee690174b1f3e3386d5125840ff92efbc31139
dm1try <me@dmitry.it>
2023-05-02 00:13:21 +0200

    start crawling on service start

--- apps/crawler/main.rb
+++ apps/crawler/main.rb
@@ -3,4 +3,7 @@ require 'drb'
 require 'drb_service'
 require 'crawler_service'
 
-DRbService.new(ENV.fetch("CRAWLER_DRB_URI"), CrawlerService.new).start
+DRbService.new(ENV.fetch("CRAWLER_DRB_URI"), CrawlerService.new).start do |service|
+  service.start_crawling
+end
+
