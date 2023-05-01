cc8cf30f902b79a46d13339d2403c9284744cfa6
dm1try <me@dmitry.it>
2023-05-01 23:21:53 +0200

    add tg_bot in console helper

--- console.rb
+++ console.rb
@@ -4,6 +4,7 @@ Dotenv.load
 
 @crawler = DRbObject.new_with_uri(ENV['CRAWLER_DRB_URI'])
 @catalog = DRbObject.new_with_uri(ENV['CATALOG_DRB_URI'])
+@tg_bot = DRbObject.new_with_uri(ENV['TG_BOT_DRB_URI'])
 
 def crawler
   @crawler
@@ -11,4 +12,8 @@ end
 
 def catalog
   @catalog
+end
+
+def tg_bot
+  @tg_bot
 end
