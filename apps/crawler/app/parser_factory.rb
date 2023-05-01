d440b37328840d7e79f05a7d681eb2a324bfd195
dm1try <me@dmitry.it>
2023-05-01 12:58:22 +0200

    add missing slash to prevent extra redirects

--- apps/crawler/app/parser_factory.rb
+++ apps/crawler/app/parser_factory.rb
@@ -4,7 +4,7 @@ class ParserFactory
   REGISTERED_PARSERS = [Olx]
 
   def self.new_for(city, filters)
-    url = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}"
+    url = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}/"
 
     REGISTERED_PARSERS.map do |parser|
       parser.new(url)
