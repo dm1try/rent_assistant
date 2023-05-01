2c2851e907ef4d02e3d294f6cfb2dd128b6db32e
dm1try <me@dmitry.it>
2023-05-01 14:07:17 +0200

    handle the case with epmpy filters

--- apps/crawler/app/search.rb
+++ apps/crawler/app/search.rb
@@ -9,9 +9,12 @@ class Search
     DB[:searches].all.each_with_object([]) do |search, found_search_ids|
       filters = JSON.parse(search[:filters], symbolize_names: true)
 
-      if listing[:price] && (listing[:price] >= filters[:price][:min] && listing[:price] <= filters[:price][:max])
-        found_search_ids << search[:id]
+      if (listing[:price] && filters[:price]) &&
+        !(listing[:price] >= filters[:price][:min] && listing[:price] <= filters[:price][:max])
+        next
       end
+
+      found_search_ids << search[:id]
     end
   end
 end
--- apps/crawler/spec/search_spec.rb
+++ apps/crawler/spec/search_spec.rb
@@ -40,5 +40,29 @@ RSpec.describe Search do
         expect(Search.percolate(not_matching_listing)).to eq([])
       end
     end
+
+    context 'when filter is not set' do
+      let(:listing) {
+        {
+          url: 'https://www.olx.pl/d/oferta/mieszkanie-3-pok-od-teraz-CID3-IDUlcmq.html',
+          address: 'Małopolskie, Kraków, Stare Miasto',
+          price: 5500,
+          location: [50.06026, 19.9396],
+          area: 107,
+          rooms: 3,
+          description: "3 pokojowe mieszkanie<br />\nPiętro: 3/5, powierzchnia: 107,5 m2<br />\nSypialnie: 2<br />\nMieszkanie po remoncie",
+          images: ["https://ireland.apollo.olxcdn.com:443/v1/files/zjknxnzhnykk-PL/image;s=1179x819", "https://ireland.apollo.olxcdn.com:443/v1/files/989bglpta90f-PL/image;s=1179x799", "https://ireland.apollo.olxcdn.com:443/v1/files/iybwjgsxp1fd2-PL/image;s=1179x819", "https://ireland.apollo.olxcdn.com:443/v1/files/llkysoug6wbk2-PL/image;s=1179x816", "https://ireland.apollo.olxcdn.com:443/v1/files/vus5sc0me7r8-PL/image;s=1179x806", "https://ireland.apollo.olxcdn.com:443/v1/files/3wqudlsodudj3-PL/image;s=1179x819", "https://ireland.apollo.olxcdn.com:443/v1/files/3f8ctleal9jm3-PL/image;s=620x367"],
+          source: {:created_at=>"2023-04-30T13:18:50+02:00", :id=>832527222, :updated_at=>"2023-04-30T13:20:58+02:00"}
+        }
+      }
+
+      before do
+        Search.create({})
+      end
+
+      it 'returns all searches' do
+        expect(Search.percolate(listing)).to eq([DB[:searches].first[:id]])
+      end
+    end
   end
 end
