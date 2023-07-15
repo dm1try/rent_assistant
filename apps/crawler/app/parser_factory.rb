d7caa7941f9ff0ffdf5864f4c87e47a8d988721f
dm1try <me@dmitry.it>
2023-07-15 19:50:40 +0200

    build specific otodom url for a city to prevent extra redirections

--- apps/crawler/app/parser_factory.rb
+++ apps/crawler/app/parser_factory.rb
@@ -3,9 +3,18 @@ require 'parser/otodom'
 
 class ParserFactory
   def self.new_for(city, filters = {})
+    otodom_url = 
+      case city.downcase
+      when 'warszawa'
+        "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/mazowieckie/warszawa/warszawa/warszawa?page=1&limit=36&by=DEFAULT&direction=DESC&viewType=listing"
+      when 'krakow'
+        "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/malopolskie/krakow/krakow/krakow?page=1&limit=36&by=DEFAULT&direction=DESC&viewType=listing"
+      else
+        "https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/#{city.downcase}?page=1&limit=36"
+      end
     [
       Olx.new("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}/"),
-      Otodom.new("https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/#{city.downcase}?page=1&limit=36")
+      Otodom.new(otodom_url)
     ]
   end
 end
