ee7ce1524169d51dc143f1c782df76f384d9bc91
dm1try <me@dmitry.it>
2023-07-15 19:11:21 +0200

    enable otodom

--- apps/crawler/app/parser_factory.rb
+++ apps/crawler/app/parser_factory.rb
@@ -4,8 +4,8 @@ require 'parser/otodom'
 class ParserFactory
   def self.new_for(city, filters = {})
     [
-      Olx.new("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}/")
-     # Otodom.new("https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/#{city.downcase}?page=1&limit=36")
+      Olx.new("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}/"),
+      Otodom.new("https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/#{city.downcase}?page=1&limit=36")
     ]
   end
 end
