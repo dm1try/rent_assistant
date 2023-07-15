01bb86b5d408263c8e12105ec50ac6c49f789bd3
dm1try <me@dmitry.it>
2023-07-15 15:53:03 +0200

    register otodom parser

--- apps/crawler/app/parser_factory.rb
+++ apps/crawler/app/parser_factory.rb
@@ -1,13 +1,10 @@
 require 'parser/olx'
 
 class ParserFactory
-  REGISTERED_PARSERS = [Olx]
-
   def self.new_for(city, filters = {})
-    url = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}/"
-
-    REGISTERED_PARSERS.map do |parser|
-      parser.new(url)
-    end
+    [
+      Olx.new("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/#{city.downcase}/"),
+      Otodom.new("https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/#{city.downcase}?page=1&limit=36")
+    ]
   end
 end
